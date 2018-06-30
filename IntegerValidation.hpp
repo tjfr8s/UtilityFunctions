bbee25ec2dc2892d54b31ee1e9d5f3abf3d7646c
Tyler Freitas <tjfr8s@gmail.com>
2018-06-29 19:34:44 -0700

    Implement integer validation function that confirms an integer was entered between the bounds.

---
+++ IntegerValidation.hpp
@@ -0,0 +1,8 @@
+#ifndef INTEGERVALIDATION_HPP
+#define INTEGERVALIDATION_HPP
+#include <iostream>
+#include <limits>
+
+int intInputValidation(int min, int max);
+
+#endif
---
+++ IntegerValidationTest.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+#include "IntegerValidation.hpp"
+
+int main()
+{
+    int integer = intInputValidation(2,7);
+    return 0;
+}
